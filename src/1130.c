c428ba6052fa7e93c34d5d6cd81030f3293e0399
Rainchus <rainmachus@gmail.com>
2024-09-01 18:40:03 -0500

    add rodata splits, wip importing (still doesn't build)

--- src/1130.c
+++ src/1130.c
@@ -1,23 +1,23 @@
 #include "common.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/1130/func_80000530_1130.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/1130/nnScCreateScheduler.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/1130/func_8000062C_122C.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/1130/nnScStartScheduler.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/1130/func_800006F8_12F8.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/1130/nnScGetAudioMQ.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/1130/func_80000704_1304.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/1130/nnScGetGfxMQ.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/1130/func_80000710_1310.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/1130/nnScEventHandler.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/1130/func_800007D4_13D4.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/1130/nnScAddClient.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/1130/func_80000824_1424.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/1130/nnScRemoveClient.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/1130/func_800008B0_14B0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/1130/func_80000900_1500.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/1130/nnScExecuteAudio.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/1130/func_80000A80_1680.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/1130/nnScExecuteGraphics.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/1130/func_80000BB4_17B4.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/1130/nnScWaitTaskReady.s")
