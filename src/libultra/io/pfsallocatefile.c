ea4b138f5d6db4499bb45d7a8103b2f8a7c365aa
Rainchus <rainmachus@gmail.com>
2025-03-28 04:45:29 -0500

    remove static so symbol appears

--- src/libultra/io/pfsallocatefile.c
+++ src/libultra/io/pfsallocatefile.c
@@ -3,7 +3,7 @@
 
 #define ROUND_UP_DIVIDE(numerator, denominator) (((numerator) + (denominator)-1) / (denominator))
 
-static s32 __osClearPage(OSPfs *pfs, int page_num, u8 *data, u8 bank);
+s32 __osClearPage(OSPfs *pfs, int page_num, u8 *data, u8 bank);
 
 s32 osPfsAllocateFile(OSPfs* pfs, u16 company_code, u32 game_code, u8* game_name, u8* ext_name, int file_size_in_bytes,
                       s32* file_no) {
@@ -168,7 +168,7 @@ s32 __osPfsDeclearPage(OSPfs* pfs, __OSInode* inode, int file_size_in_pages, int
     }
 }
 
-static s32 __osClearPage(OSPfs *pfs, int page_no, u8 *data, u8 bank)
+s32 __osClearPage(OSPfs *pfs, int page_no, u8 *data, u8 bank)
 {
     int i;
     s32 ret;
